4e6765ed3639f982f7f741e41bcfc910d30262b3
Kieran Llarena <baymax709@gmail.com>
2024-07-22 19:05:44 -0400

    chore: added comments for better readability

--- main.s
+++ main.s
@@ -2,17 +2,17 @@
 .align 4
 
 _main:
-  mov X0, #1
-  adr X1, firstmessage
-  mov X2, #24
-  mov X16, #4
-  svc 0
+  mov X0, #1              // stdout
+  adr X1, firstmessage    // address of first message
+  mov X2, #24             // number of bytes of the first message
+  mov X16, #4             // write to stdout
+  svc 0                   // syscall
 
   b _terminate
 
 _terminate:
-  mov X0, #0
-  mov X16, #1
-  svc 0
+  mov X0, #0              // return 0
+  mov X16, #1             // terminate
+  svc 0                   // syscall
 
 firstmessage: .ascii "Enter the first number:\n"
