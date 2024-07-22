d7973da8d0ff25bffdc01d142a2b7b07edf95720
Kieran Llarena <baymax709@gmail.com>
2024-07-22 18:56:01 -0400

    feat: print first message

--- main.s
+++ main.s
@@ -2,9 +2,17 @@
 .align 4
 
 _main:
+  mov X0, #1
+  adr X1, firstmessage
+  mov X2, #24
+  mov X16, #4
+  svc 0
+
   b _terminate
 
 _terminate:
   mov X0, #0
   mov X16, #1
   svc 0
+
+firstmessage: .ascii "Enter the first number:\n"
