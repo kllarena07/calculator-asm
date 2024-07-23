4718a17cde3464cae33eacfd4566f6041ba8e751
Kieran Llarena <baymax709@gmail.com>
2024-07-22 20:35:39 -0400

    feat: get the input of the 2nd number

--- main.s
+++ main.s
@@ -9,17 +9,24 @@ _main:
   svc 0                         ; syscall
 
   mov x0, #0                    ; stdin
-  adrp x1, buffer@PAGE
-  add x1, x1, buffer@PAGEOFF    ; load address of buffer
+  adrp x1, firstbuffer@PAGE
+  add x1, x1, firstbuffer@PAGEOFF    ; load address of buffer
   mov x2, #100                  ; max number of bytes to read
   mov x16, #3                   ; syscall number for read
   svc 0                         ; make syscall
 
-  mov x0, #1
-  adr x1, buffer
-  mov x2, #100
-  mov x16, #4
-  svc 0
+  mov x0, #1                    ; stdout
+  adr x1, secondmessage          ; address of second message
+  mov x2, #25                   ; number of bytes of the first message
+  mov x16, #4                   ; write to stdout
+  svc 0                         ; syscall
+
+  mov x0, #0                    ; stdin
+  adrp x1, secondbuffer@PAGE
+  add x1, x1, secondbuffer@PAGEOFF    ; load address of buffer
+  mov x2, #100                  ; max number of bytes to read
+  mov x16, #3                   ; syscall number for read
+  svc 0                         ; make syscall
 
   b _terminate
 
@@ -29,5 +36,7 @@ _terminate:
   svc 0                         ; syscall
 
 firstmessage: .ascii "Enter the first number:\n"
+secondmessage: .ascii "Enter the second number:\n"
 .data
-buffer: .space 100
+firstbuffer: .space 100
+secondbuffer: .space 100
