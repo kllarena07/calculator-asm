b2de8ef9c754907a286c7ad84671e87f3ebbba72
Kieran Llarena <baymax709@gmail.com>
2024-07-26 15:10:30 -0400

    fix: print instructions and get user input

--- main.s
+++ main.s
@@ -2,41 +2,53 @@
 .align 4
 
 _main:
-  mov x0, #1                          ; stdout
-  adr x1, firstmessage                ; address of first message
-  mov x2, #24                         ; number of bytes of the first message
-  mov x16, #4                         ; write to stdout
-  svc 0                               ; syscall
+    mov x0, #1                          ; stdout
+    adrp x1, first_message@PAGE         ; address of first message
+    add x1, x1, first_message@PAGEOFF
+    mov x2, #24                         ; number of bytes of the first message
+    mov x16, #4                         ; write to stdout
+    svc 0                               ; syscall
 
-  mov x0, #0                          ; stdin
-  adrp x1, firstbuffer@PAGE           ; calculate the offset
-  add x1, x1, firstbuffer@PAGEOFF     ; load address of buffer
-  mov x2, #100                        ; max number of bytes to read
-  mov x16, #3                         ; syscall number for read
-  svc 0                               ; make syscall
+    mov x0, #0                          ; stdin
+    adrp x1, first_buffer@PAGE          ; calculate the offset
+    add x1, x1, first_buffer@PAGEOFF    ; load address of buffer
+    mov x2, #2                          ; max number of bytes to read
+    mov x16, #3                         ; syscall number for read
+    svc 0                               ; make syscall
 
-  mov x0, #1                          ; stdout
-  adr x1, secondmessage               ; address of second message
-  mov x2, #25                         ; number of bytes of the first message
-  mov x16, #4                         ; write to stdout
-  svc 0                               ; syscall
+    mov x0, #1                          ; stdout
+    adrp x1, second_message@PAGE        ; address of second message
+    add x1, x1, second_message@PAGEOFF
+    mov x2, #25                         ; number of bytes of the first message
+    mov x16, #4                         ; write to stdout
+    svc 0                               ; syscall
 
-  mov x0, #0                          ; stdin
-  adrp x1, secondbuffer@PAGE          ; calculate the offset
-  add x1, x1, secondbuffer@PAGEOFF    ; load address of buffer
-  mov x2, #100                        ; max number of bytes to read
-  mov x16, #3                         ; syscall number for read
-  svc 0                               ; make syscall
+    mov x0, #0                          ; stdin
+    adrp x1, second_buffer@PAGE         ; calculate the offset
+    add x1, x1, second_buffer@PAGEOFF   ; load address of buffer
+    mov x2, #2                          ; max number of bytes to read
+    mov x16, #3                         ; syscall number for read
+    svc 0                               ; make syscall
 
-  b _terminate
+    ;mov x0, #1                          ; stdout
+    ;adrp x9, first_buffer@PAGE
+    ;add x9, x9, first_buffer@PAGEOFF
+    ;adrp x10, second_buffer@PAGE
+    ;add x10, x10, second_buffer@PAGEOFF
+    ;add x1, x9, x10
+    ;mov x2, #100                        ; number of bytes
+    ;mov x16, #4                         ; write to stdout
+    ;svc 0                               ; syscall
+
+    b _terminate
 
 _terminate:
-  mov x0, #0                          ; return 0
-  mov x16, #1                         ; terminate
-  svc 0                               ; syscall
+    mov x0, #0                          ; return 0
+    mov x16, #1                         ; terminate
+    svc 0                               ; syscall
 
-firstmessage: .ascii "Enter the first number:\n"
-secondmessage: .ascii "Enter the second number:\n"
 .data
-firstbuffer: .space 100
-secondbuffer: .space 100
+    first_message: .ascii "Enter the first number:\n"
+    second_message: .ascii "Enter the second number:\n"
+    first_buffer: .space 1
+    second_buffer: .space 1
